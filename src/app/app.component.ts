b507cfc21284ac6dccb9a6dd28cbd9576f4bc3c4
Yissef-Makhlouf <yussef.ali.it@gmail.com>
2024-07-23 04:19:34 +0300

    add new repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { AboutComponent } from './about/about.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioDetailComponent } from './portfolio-detail/portfolio-detail.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroComponent,FooterComponent,AboutComponent,SkillsComponent,PortfolioDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
+
 }
---
+++ src/app/portfolio-detail/portfolio-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-detail.component.html',
+  styleUrl: './portfolio-detail.component.css'
+})
+export class PortfolioDetailComponent {
+
+}
